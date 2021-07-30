6707a257e17a96d987d14b79791a71297f18cb3d
zhaozhentao <qq344696734@gmail.com>
2021-07-30 17:48:02 +0800

    🎨修改cards返回

--- app/models/card/card.go
+++ app/models/card/card.go
@@ -2,11 +2,21 @@ package card
 
 import (
 	"buhang/bootstrap"
+	"time"
 )
 
 type Card struct {
-	ID         uint64 `gorm:"column:id;primaryKey;autoIncrement;not null"`
-	CategoryId uint64 `gorm:"column:category_id"`
+	ID           uint64    `json:"id"`
+	CategoryId   uint64    `json:"category_id"`
+	Img          string    `json:"img"`
+	GoodsNumber  string    `json:"goods_number"`
+	GoodsName    string    `json:"goods_name"`
+	Weight       string    `json:"weight"`
+	MeterPerKilo float32   `json:"meter_per_kilo"`
+	Width        float32   `json:"width"`
+	Sort         int       `json:"sort"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
 }
 
 func (Card) TableName() string {
@@ -15,8 +25,10 @@ func (Card) TableName() string {
 
 func Index() []Card {
 	var cards []Card
+
 	if err := bootstrap.DB.Find(&cards).Error; err != nil {
 		return cards
 	}
+
 	return cards
 }
